dd6f495015d5af712e19546f592ff243775b4ecf
SaouZADEV <82593060+SaouZADEV@users.noreply.github.com>
2022-03-26 09:36:43 +0700

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -144,9 +144,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+MEDIA_URL = 'media/'
 
 #MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "files/")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files/')
